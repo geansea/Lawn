c71f2ca7593760483a83af977f1f9d283789a98c
geansea <geansea@126.com>
2021-02-20 21:44:40 +0800

    Create basic layout for home

--- app/src/main/java/com/geansea/lawn/activity/HomeActivity.kt
+++ app/src/main/java/com/geansea/lawn/activity/HomeActivity.kt
@@ -5,20 +5,33 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.FrameLayout
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.gridlayout.widget.GridLayout
 import com.geansea.lawn.R
 import com.geansea.lawn.util.device.DeviceUtils
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var container: FrameLayout
+    private lateinit var container: ViewGroup
+    private lateinit var cells: GridLayout
+    private lateinit var bottomBar: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         container = findViewById(R.id.container)
+        cells = findViewById(R.id.cells)
+        bottomBar = findViewById(R.id.bottom_bar)
+    }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        enableFullscreen()
+    }
+
+    private fun enableFullscreen() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         val uiNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         val statusBarLightMode = uiNightMode != Configuration.UI_MODE_NIGHT_YES
